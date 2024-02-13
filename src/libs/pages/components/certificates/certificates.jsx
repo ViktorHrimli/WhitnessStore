3d75b4b86edac3bba3ceac5722065b038dbf4efd
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2024-02-14 00:04:21 +0200

    make created certificated

--- src/libs/pages/components/certificates/certificates.jsx
+++ src/libs/pages/components/certificates/certificates.jsx
@@ -1,70 +1,98 @@
-"use client"
+"use client";
 
-import { useState } from 'react'
+import { usePerfectState } from "@/shared/shared";
 
-import styles from './certificates.module.scss'
-import Basket from '@/libs/components/basket/basket';
-import sertificat from '@/assets/svg/sertificat-2.png'
+import styles from "./certificates.module.scss";
+import Basket from "@/libs/components/basket/basket";
+import sertificat from "@/assets/svg/sertificat-2.png";
 
+var Sertificates = () => {
+  var [isOpen, setIsOpen] = usePerfectState(false);
+  var [inputValue, setInputValue] = usePerfectState("");
 
+  // const CardTitle = "Zertifikat";
+  // const CardPrice = inputValue;
+  // const CardImg = [
+  //   {
+  //     original: sertificat,
+  //   },
+  // ];
 
-export default function Sertificates() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [inputValue, setInputValue] = useState('');
+  // const satz = "";
+  // const color = "";
+  // const addition = "";
+  // const isChestCircumference = "";
+  // const isUnderbustMeasurement = "";
+  // const isHipCircumference = "";
+  // const isTaillenumfang = "";
 
-  const CardTitle = "Zertifikat";
-  const CardPrice = inputValue;
-  const CardImg = [
-    {
-      original: sertificat,
-    }
-  ];
-
-  const satz = "";
-  const color = "";
-  const addition = "";
-  const isChestCircumference = "";
-  const isUnderbustMeasurement = "";
-  const isHipCircumference = "";
-  const isTaillenumfang = "";
-
-
-  
-  const handleClick = (event) => {
+  var handleClick = async (event) => {
     event.preventDefault();
-    setIsOpen(true);
+    // setIsOpen(true);
 
-    const existingData = JSON.parse(localStorage.getItem('storedItems')) || [];
+    await fetch("https://whitness-store.online/api/certificates", {
+      method: "POST",
+      body: JSON.stringify({
+        data: {
+          activated: false,
+          used: "",
+          amount: +inputValue,
+          title: "TEST",
+        },
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).catch((e) => {
+      console.log(e.message);
+    });
 
-    const newCardData = { CardTitle: CardTitle, CardPrice: CardPrice, CardImg: CardImg, satz, color, addition, isChestCircumference, isUnderbustMeasurement, isHipCircumference, isTaillenumfang};
+    // const existingData = JSON.parse(localStorage.getItem("storedItems")) || [];
 
-    const updatedData = [...existingData, newCardData];
+    // const newCardData = {
+    //   CardTitle: CardTitle,
+    //   CardPrice: CardPrice,
+    //   CardImg: CardImg,
+    //   satz,
+    //   color,
+    //   addition,
+    //   isChestCircumference,
+    //   isUnderbustMeasurement,
+    //   isHipCircumference,
+    //   isTaillenumfang,
+    // };
 
-    localStorage.setItem('storedItems', JSON.stringify(updatedData));
-    // localStorage.setItem('storedItems', JSON.stringify([{CardTitle , CardPrice}]));
-    setInputValue('');
+    // const updatedData = [...existingData, newCardData];
+
+    // localStorage.setItem("storedItems", JSON.stringify(updatedData));
+
+    setInputValue("");
   };
 
   return (
-    <>
-    <section className={styles.section}>
-      <div className={styles.container}>
-        <p className={styles.title}>{CardTitle}</p>
-        <p className={styles.text}>Drucken Sie auf jeden Betrag ab 50 Euro</p>
-        <form action="" className={styles.form}>
-            <input className={styles.input}
+    <article>
+      <section className={styles.section}>
+        <div className={styles.container}>
+          <p className={styles.title}>Zertifikat</p>
+          <p className={styles.text}>Drucken Sie auf jeden Betrag ab 50 Euro</p>
+          <form className={styles.form}>
+            <input
+              className={styles.input}
               type="text"
-              placeholder='50'
+              placeholder="50"
               required
               value={inputValue}
               onChange={(e) => setInputValue(e.target.value)}
             />
-            
-          <button className={styles.btn} onClick={handleClick}>Kaufen</button>
-        </form>
-      </div>
-    </section>
-      {isOpen && <Basket />}
-      </>
-  )
-}
+            <button className={styles.btn} onClick={handleClick}>
+              Kaufen
+            </button>
+          </form>
+        </div>
+      </section>
+      {isOpen && <Basket storedItems={[]} setStoredItems={[]} />}
+    </article>
+  );
+};
+
+export default Sertificates;
